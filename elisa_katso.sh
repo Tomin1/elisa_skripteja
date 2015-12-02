f8f21ad621152487e6aeaf4bfe731eb3897e65b9
Tomi Leppänen <tomi.leppanen@telemail.fi>
2015-12-02 22:40:50 +0200

    elisa_katso.sh: Korjattu inotifywaitin vaatimukset
    Lisätty tarkastus sille, että inotifywaitin pitää tukea --include vipua
    tai sitä ei käytetä. Jos joku jaksaa, niin voisi tehdä tuen myös
    muokkaamattomalle inotifywaitille.

--- elisa_katso.sh
+++ elisa_katso.sh
@@ -2,6 +2,7 @@
 # Public domain
 # Requires: elisa_url.py, vlc, wget
 # Recommends: inotifywait (inotify-tools)
+# Note: inotifywait must support --include option
 # Use: elisa_katso.sh [arguments_to_wget] \
 #          url_of_the_video_page_in_elisa_viihde
 TMP_FILE="/tmp/elisa-viihde-$$.mpg"
@@ -11,11 +12,13 @@ if [ "$?" -ne "0" ]; then
     echo "Ei voitu hakea osoitetta videolle!"
     exit 2
 fi
-if [ -z $(which inotifywait 2> /dev/null) ]; then
-    sh -c "sleep 10 && vlc $TMP_FILE" &
-else
-    sh -c "inotifywait -e create --include $TMP_FILE ${TMP_FILE%/*} && vlc $TMP_FILE" &
+if [ -n "$(which inotifywait 2> /dev/null)" && \
+        -n "$(inotifywait --help |grep include)" ]; then
+    sh -c "inotifywait -e create --include $TMP_FILE ${TMP_FILE%/*} \
+        && vlc $TMP_FILE" &
     sleep 1
+else
+    sh -c "sleep 10 && vlc $TMP_FILE" &
 fi
 wget ${@: 1:${#@}-1} -O "$TMP_FILE" "$URL"
 wait && rm "$TMP_FILE"

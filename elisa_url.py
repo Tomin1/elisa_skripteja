178ed09d95f6297cd8d9ae6124d19f2a314cd7cb
Tomi Leppänen <Tomin1@users.noreply.github.com>
2017-11-22 18:18:15 +0200

    Päivitetty Copyright-rivi

--- elisa_url.py
+++ elisa_url.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 # 
 # Elisa Viihde url hakija
-# Copyright (c) Tomi Leppänen, 2014-2015
+# Copyright (c) Tomi Leppänen, 2014-2017
 # 
 # Permission is hereby granted, free of charge, to any person obtaining a copy
 # of this software and associated documentation files (the "Software"), to deal
